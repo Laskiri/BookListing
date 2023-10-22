32b934d502368b4e7d7eac7fc60a0250d1fdf1ef
Lasse Drongesen <Lassemgp@gmail.com>
2023-10-22 14:37:39 +0200

    small adjustments

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -3,9 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { BsInfoCircle } from 'react-icons/bs'
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import { MdOutlineAddBox} from 'react-icons/md'
 import BooksCard from '../components/home/BooksCard'
 import BooksTable from '../components/home/BooksTable'
 
@@ -30,7 +28,9 @@ const Home = () => {
 
     return (
         <div className='p-4'>
-            <div className='flex justify-center items-center gap-x-4'>
+            <div className='flex justify-evenly  flex-col items-center'>
+                <h1 className='text-5xl my-6 font-medium'>Book List</h1>
+                <div className='flex justify-center items-center gap-x-4'>
                 <button
                     className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
                     onClick={() => { setShowType('table') }}>
@@ -41,13 +41,13 @@ const Home = () => {
                     onClick={() => { setShowType('card') }}>
                     Card
                 </button>
-            </div>
-            <div className='flex justify-between items-center'>
-                <h1 className='text-3xl my-8'>Book List</h1>
+                </div>
                 <Link to='/books/create'>
-                    <MdOutlineAddBox className='text-skye-800 text-4xl' />
+                    <MdOutlineAddBox className='text-skye-800 text-4xl my-4' />
                 </Link>
             </div>
+            
+            
             {loading ? <Spinner /> : showType === 'table' ? (<BooksTable books={books} />) : (<BooksCard books={books} />)}
         </div>
     )
